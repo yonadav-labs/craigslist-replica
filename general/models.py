f01ab38f62a3976b307d8a0a5fb754f262fb30b1
yonadav-labs <yonadav.barilan.upwork@gmail.com>
2017-11-18 19:38:19 -0600

    add description for user

--- general/models.py
+++ general/models.py
@@ -25,7 +25,8 @@ class Customer(AbstractUser):
     duration = models.CharField(max_length=100, blank=True, null=True)
     v_statue = models.CharField(max_length=50)
     id_photo = models.ImageField(blank=True, null=True)
-
+    description = models.TextField(blank=True, null=True)
+    
     def __str__(self):
         return self.username
 
