8306a81e7723a4ded9e2d31a03c720735766e02d
yonadav-labs <yonadav.barilan.upwork@gmail.com>
2017-11-29 01:48:37 -0600

    finish custom signup

--- general/adapter.py
+++ general/adapter.py
@@ -10,23 +10,3 @@ class MyAccountAdapter(DefaultAccountAdapter):
             path = "/profile/#" + default_site
         return path
 
-    def save_user(self, request, user, form, commit=False):
-        data = form.cleaned_data
-        user.email = data['email']
-        user.first_name = data['first_name']
-        user.last_name = data['last_name']
-        user.gender = data['gender']
-        user.dob = data['dob']
-        user.address = data['address']
-
-        if 'password1' in data:
-            user.set_password(data['password1'])
-        else:
-            user.set_unusable_password()
-
-        self.populate_username(request, user)
-
-        print user, '########3'
-        if commit:
-            user.save()
-        return user
--- general/forms.py
+++ general/forms.py
@@ -7,14 +7,38 @@ from django.forms.utils import ErrorList
 from django.forms.formsets import formset_factory
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from allauth.utils import generate_unique_username
 
 from .models import *
 
 class SignupForm(UserCreationForm):
+    def signup(self, request, user):
+        data = self.cleaned_data
+        user.email = data['email']
+        user.first_name = data['first_name']
+        user.last_name = data['last_name']
+        user.gender = data['gender']
+        user.dob = data['dob']
+        user.address = data['address']
+
+        if 'password1' in data:
+            user.set_password(data['password1'])
+        else:
+            user.set_unusable_password()
+
+        user.username = generate_unique_username([
+            data['first_name'],
+            data['last_name'],
+            data['email'],
+            'user'])
+
+        user.save()
+        return user
+
     class Meta:
         model = get_user_model()
-        fields = ('username', 'email', 'password1', 'password2', 
-                  'first_name', 'last_name', 'gender', 'dob', 'address')
+        fields = ('email', 'password1', 'password2', 'first_name', 
+                  'last_name', 'gender', 'dob', 'address')
 
 
 class PostForm(ModelForm):
